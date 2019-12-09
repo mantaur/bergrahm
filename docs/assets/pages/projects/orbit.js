58c8abea71d01cbe144a86340767859ba1545dbe
Sabewey <44124418+Sabewey@users.noreply.github.com>
2019-12-09 22:16:37 +0100

    Update orbit.js
    update orbit

--- docs/assets/pages/projects/orbit.js
+++ docs/assets/pages/projects/orbit.js
@@ -1,34 +1,49 @@
-//debugger
-
-//Setting up Canvas - Visualize
 /*
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-canvas.height = window.innerHeight - 200;
-canvas.width = window.innerWidth - 100;
-*/          //Converting to SVG.js
-
-
+USING sun and earth:
+Sun M: 1.989 * Math.pow(10, 30)
+Earth's M: 5.972 * Math.pow(10, 24)
+Earth's orbital speed: 30 * Math.pow(10, 3)
+Earth's distance: 149.6 * Math.pow(10, 9)
+G = 6.67408 * Math.pow(10, -11)
+*/
 
 //Smaller planets cordinates
-x = 2;//3.7 * Math.pow(10, 8);
-y = 0.1;
+x = 149.6 * Math.pow(10, 9);
+y = 0.1
+
+//Larger planet cord
+x2 = 0;
+y2 = 0;
 
 //Variables
-G = 1//6.67408 * Math.pow(10, -11);
-M = 1//5.9272 * Math.pow(10, 24);         //mass in kg
-m = 1//7.35 * Math.pow(10, 22);           //mass in kg
+G = 6.67408 * Math.pow(10, -11);
+m = 5.972 * Math.pow(10, 30);        //mass in kg
+M = 1.989 * Math.pow(10, 30);         //mass in kg
 
-dt = 0.1;
+// Step-size
+dt = 10000;
 
 //Smaller x-y speed
 Xs = 0;
-Ys = 1//4 * Math.pow(10, 2); // 400 m/s
+Ys = 30 * Math.pow(10, 3);
 
-//Forces:
+//Larger x-y speed
+Xs2 = 0;
+Ys2 = 0;
+
+//Smallers Forces:
 Fx = 0;
 Fy = 0;
+
+//Largers forces:
+Fx2 = 0;
+Fy2 = 0;
+
+
+
+A = 0;
+B = 0;
+C = 0;
 
 w = window.innerWidth.toString();
 h = window.innerHeight.toString();
@@ -38,7 +53,7 @@ svg.setAttribute('id', 'svg-container');
 svg.setAttribute('style', 'border: 1px solid black');
 svg.setAttribute('width', w);
 svg.setAttribute('height', h);
-svg.setAttribute('viewBox', '-600 -1000 3000 3000');
+svg.setAttribute('viewBox', '-2000 -2000 4000 4000');
 svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
 document.body.appendChild(svg);
 
@@ -47,157 +62,60 @@ var VB = svg.getAttribute('viewBox').split(' ').map(Number);
 
 window.addEventListener('keyup', (key) => {
     if (key.keyCode === 37) {
-        VB[0] -= 100;
+        VB[0] -= 1000;
         svg.setAttribute('viewBox', VB);
     }
 
     if (key.keyCode === 39) {
-        VB[0] += 100;
+        VB[0] += 1000;
         svg.setAttribute('viewBox', VB);
     }
 
     if (key.keyCode === 38) {
-        VB[1] -= 100;
+        VB[1] -= 1000;
         svg.setAttribute('viewBox', VB);
     }
 
     if (key.keyCode === 40) {
-        VB[1] += 100;
+        VB[1] += 1000;
         svg.setAttribute('viewBox', VB);
     }
 
     //Zooming in and out
     if (key.keyCode === 187) {
-        VB[2] += 500;
-        VB[3] += 500;
+        VB[2] += 1000;
+        VB[3] += 1000;
         svg.setAttribute('viewBox', VB);
+        console.log(VB);
+        console.log(x / 100000000);
     }
 
     if (key.keyCode === 189) {
-        VB[2] -= 300;
-        VB[3] -= 300;
+        VB[2] -= 1000;
+        VB[3] -= 1000;
         svg.setAttribute('viewBox', VB);
+        console.log(VB);
+        console.log(x / 100000000 + "y:" + y / 100000000);
     }
 });
 
 
-console.log(VB);
-
-//flag for for loop
-var One = 0;
-var flag = 0;
-
-function oneEuler() {
-
-    for (let i = 0; flag < 1; i++) {
-
-        //useful math var
-        x2 = Math.pow(x, 2);          // x to the power of 2
-        y2 = Math.pow(y, 2);          // y to the power of 2
-        xy2 = x2 / y2;                // used later for acceleration
-        yx2 = y2 / x2;                // used later for acceleration
-        r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)); //distance between planets
-
-        //console.log(r + "radius is working");
-
-        //Calculate the y-acceleration:
-        Fg = Math.pow(G, 2) * Math.pow(M, 2) * Math.pow(m, 2) / (x2 + y2); // Newtons gravitational law - can reuse for ay
-
-        Fy2 = Fg / (xy2 + 1);
-        Fy = Math.sqrt(Fy2);
-        //Give the force a riktning:
-        Fy = Fy * (-y / Math.abs(y));
-        ay = Fy / m;
-
-        //Calculate the y-acceleration:
-        Fx2 = Fg / (yx2 + 1);
-        Fx = Math.sqrt(Fx2);
-        //Give the force a riktning
-        Fx = Fx * (-x / Math.abs(x));
-        ax = Fx / m;
-
-        //Stopping after one orbit
-        One = y;
-
-        //
-        // ACTUAL EULERS METHOD
-        //
-
-        var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-        rect.setAttribute('x', x * 500);
-        rect.setAttribute('y', y * 300);
-        rect.setAttribute('width', 10);
-        rect.setAttribute('height', 10);
-        rect.setAttribute('fill', '#95B3D7');
-        svg.appendChild(rect);
-        document.body.appendChild(svg);
-
-
-        x = x + Xs * dt;
-        y = y + Ys * dt;
-
-        Xs = Xs + ax * dt;
-        Ys = Ys + ay * dt;
-
-
-        console.log("x - pos:" + x);
-        console.log("y - pos:" + y);
-
-        // Stop after one orbit
-        if (One < 0 && y > 0) flag = 1;
-    }
-
-}
-
-
-
-
-
-
-
-
-
 function euler() {
-    flag = 0;
-    for (let i = 0; i < 100; i++) {
+    //use i<33000 with dt = 1000
+    for (let i = 0; i < 3300; i++) {
 
-        //useful math var
-        x2 = Math.pow(x, 2);          // x to the power of 2
-        y2 = Math.pow(y, 2);          // y to the power of 2
-        xy2 = x2 / y2;                // used later for acceleration
-        yx2 = y2 / x2;                // used later for acceleration
-        r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)); //distance between planets
+        A = (G ** 2 * M ** 2 * m ** 2) / ((x ** 2 + y ** 2) ** 2);
+        B = (((x - x2) ** 2) / ((y -y2) ** 2)) + 1;
+        C = ((y ** 2) / (x ** 2)) + 1;
+
+        Fy = Math.sqrt(A / B) * (-y / Math.abs(y));
+        Fx = Math.sqrt(A / C) * (-x / Math.abs(x));
 
 
-        //Calculate the y-acceleration:
-        Fg = Math.pow(G, 2) * Math.pow(M, 2) * Math.pow(m, 2) / (x2 + y2); // Newtons gravitational law - can reuse for ay
 
-        Fy2 = Fg / (xy2 + 1);
-        Fy = Math.sqrt(Fy2);
-        //Give the force a riktning:
-        Fy = Fy * (-y / Math.abs(y));
-        ay = Fy / m;
-
-        //Calculate the y-acceleration:
-        Fx2 = Fg / (yx2 + 1);
-        Fx = Math.sqrt(Fx2);
-        //Give the force a riktning
-        Fx = Fx * (-x / Math.abs(x));
+        //Calculating Smaller 
         ax = Fx / m;
-
-        //
-        // ACTUAL EULERS METHOD
-        //
-
-        var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-        rect.setAttribute('x', x * 500);
-        rect.setAttribute('y', y * 300);
-        rect.setAttribute('width', 10);
-        rect.setAttribute('height', 10);
-        rect.setAttribute('fill', '#95B3D7');
-        svg.appendChild(rect);
-        document.body.appendChild(svg);
-
+        ay = Fy / m;
 
         x = x + Xs * dt;
         y = y + Ys * dt;
@@ -205,10 +123,44 @@ function euler() {
         Xs = Xs + ax * dt;
         Ys = Ys + ay * dt;
 
+        //Calculating Larger
+        ax2 = -Fx / M;
+        ay2 = -Fy / M;
 
-        console.log("x - pos:" + x);
-        console.log("y - pos:" + y);
+        x2 = x2 + Xs2 * dt;
+        y2 = y2 + Ys2 * dt;
 
+        Xs2 = Xs2 + ax2 * dt;
+        Ys2 = Ys2 + ay2 * dt;
+
+        //drawing vars
+        cx = x / 100000000;
+        cy = y / 100000000;
+
+        Cx = x2 / 100000000;
+        Cy = y2 / 100000000;
+
+        //Drawing smaller
+        var rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        rect.setAttribute('x', cx);
+        rect.setAttribute('y', cy);
+        rect.setAttribute('width', 20);
+        rect.setAttribute('height', 20);
+        rect.setAttribute('fill', '#34eb34');
+        svg.appendChild(rect);
+        document.body.appendChild(svg);
+
+        //Drawing Larger
+        var rect2 = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+        rect2.setAttribute('cx', Cx);
+        rect2.setAttribute('cy', Cy);
+        rect2.setAttribute('r', 20);
+        rect2.setAttribute('fill', '#ebe134');
+        svg.appendChild(rect2);
+        document.body.appendChild(svg);
+
+        console.log("x:" + Fx);
+        console.log("y:" + Fy);
+    
     }
-
 }
